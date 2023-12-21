08c298dc61bcfb5f028174bc8c0f714ecc49ad65
Evgeniya Chernyavskaya <chev640@gmail.com>
2023-12-21 11:56:16 +0200

    feat: implement list & stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l.next) return l.value === k ? null : l;
+  let head = l;
+  if (l.value === k) head = l.next;
+  let { next } = l;
+  let previous = l;
+  while (next) {
+    if (next.value === k) {
+      previous.next = next.next;
+    } else {
+      previous = next;
+    }
+    next = next.next;
+  }
+  return head;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
